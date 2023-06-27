a107b61335d609e5756f784f79d0baa5deeec770
Spandana <spandanapasupuleti97@gmail.com>
2023-06-27 18:10:04 -0400

    Router Module Included

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './Components/Header/Header';
 import Nav from './Components/Nav/Nav';
 import About from './Components/About/About';
